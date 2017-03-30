9f99c9942e4c2e0ccd4b892ff00aeb800d111f77
yangzijun <cdyangzijun@jd.com>
2017-03-30 14:53:44 +0800

    利用zk实现选择

--- src/main/java/com/nop/App.java
+++ src/main/java/com/nop/App.java
@@ -6,23 +6,16 @@ import java.util.Random;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
+public class App {
 
     public static void getHostName() throws UnknownHostException {
         System.out.println(new String(InetAddress.getLocalHost().getCanonicalHostName().toString()));
     }
-    public static void main( String[] args )
-    {
-        try {
-            getHostName();
-            for(int i=0;i<100;i++){
-                System.out.println(new Random().nextInt(1000));
-            }
-        } catch (UnknownHostException e) {
-            e.printStackTrace();
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; ++i) {
+            System.out.println(i);
         }
     }
 }
--- src/main/java/com/nop/zk/ClientFramework/CuratorClient.java
+++ src/main/java/com/nop/zk/ClientFramework/CuratorClient.java
@@ -1,26 +1,32 @@
 package com.nop.zk.ClientFramework;
 
+import com.google.common.base.Joiner;
+import com.google.common.collect.Lists;
 import org.apache.curator.RetryPolicy;
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.CuratorFrameworkFactory;
 import org.apache.curator.retry.ExponentialBackoffRetry;
 
+import java.util.List;
+
 /**
  * Created by yangzijun on 17-3-28.
+ * 模拟启动了１０个客户端连接zk集群，在每个服务端机器上执行echo cons |nc 127.0.0.1 218x ,就可以看到每台服务器上accept到的连接。。从结果可以看出已经是做了负载的，单台机器的负载是比较均匀的，分布为4,4,6
  */
 public class CuratorClient {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         RetryPolicy retryPolicy = new ExponentialBackoffRetry(1000, 3);
-        CuratorFramework client = CuratorFrameworkFactory.newClient("127.0.0.1:2181", retryPolicy);
-        client.start();
-        try {
-            System.out.println(new String(client.getData().forPath("/")));
-            //client.getData().in
-        } catch (Exception e) {
-            e.printStackTrace();
-        }finally {
-            client.close();
+        List<String> servers = Lists.newArrayList("127.0.0.1:2181", "127.0.0.1:2182", "127.0.0.1:2183");
+        List<CuratorFramework> clients = Lists.newArrayList();
+        for (int i = 0; i < 10; ++i) {
+            CuratorFramework client = CuratorFrameworkFactory.newClient(Joiner.on(",").join(servers).toString(), retryPolicy);
+            client.start();
+            clients.add(client);
         }
 
+        Thread.sleep(10000);
+        for (int i = 0; i < 10; ++i) {
+            clients.get(i).close();
+        }
     }
 }
